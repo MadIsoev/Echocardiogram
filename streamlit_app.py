004ef1d75780a9b469f3dabab8edf150a1a4e00e
MadIsoev <isoeviso@gmail.com>
2025-02-26 15:01:58 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,20 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 
-st.title('🎈 App Name')
+st.title('Echocardiogram')
 
-st.write('Hello world!')
+st.write('Анализ набора данных Echocardiogram')
+
+df = pd.read_csv("echocardiogram.data")
+
+with st.expander('Data'):
+  st.write("X")
+  X_raw = df.drop('species', axis=1)
+  st.dataframe(X_raw)
+
+  st.write("y")
+  y_raw = df.species
+  st.dataframe(y_raw)
+
+
