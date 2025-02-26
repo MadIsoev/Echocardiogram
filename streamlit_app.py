1dca435c4aae73ecb9bf1589dbe253dfe5c3fe1e
MadIsoev <isoeviso@gmail.com>
2025-02-26 15:58:39 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.title('Echocardiogram')
+st.title('Echocardiogramm')
 
 st.write('Анализ набора данных Echocardiogramm')
 
